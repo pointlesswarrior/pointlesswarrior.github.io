723faefe545e9548579d33d9c5b6ab5de994201f
Jessie Odeh <jessieodeh08@gmail.com>
2024-04-02 00:41:36 +0200

    - fix bundling sass

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ server: { port: 3000 } });
+export default defineConfig({
+  server: { port: 3000 },
+  vite: {
+    css: {
+      transformer: "lightningcss",
+    },
+  },
+});
