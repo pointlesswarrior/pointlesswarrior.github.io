26161427104db886b9b1a81e1b2184617dafda16
Jessie Odeh <jessieodeh08@gmail.com>
2024-04-02 00:58:01 +0200

    - add sitemap, robots.txt, some meta tags, base_url for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3000 },
-  vite: {
-    css: {
-      transformer: "lightningcss",
-    },
+  server: {
+    port: 3000,
   },
+  site: "https://aryxst.github.io",
+  base: "adamkhalz-portfolio",
+  integrations: [sitemap()],
 });
